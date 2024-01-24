c49558134b8a59a86b54d6de7692daa959aaf88a
Raja Duraisamy <rajadura@cisco.com>
2024-01-24 19:01:01 +0530

    Adding base feature in main branch

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 print("Welcome to IT world") #Raja
 print("This is my second line of the program") #vinay
 print("Third line")
+
+
+
+print("You can build any calculator as base from main branch")
+
+
+print("Add feature")
+print("Sub feature")
+print("Mul feature")
+print("Div feature")
+print("Per feature")
+print("Mod feature")
